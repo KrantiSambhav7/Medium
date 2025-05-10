b87aad1019cb08a47d4a0465bda937fdc66f1b3b
KrantiSambhav7 <krantisambhav7@gmail.com>
2025-05-10 20:08:31 +0530

    Blog updated

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import BlogCard from '../component/BlogCard'
 import Appbar from '../component/Appbar'
-import { useBlogs } from '../hooks'
 import Loader from '../component/Loader'
 import axios from 'axios'
 import { useParams } from 'react-router-dom'
+import FullBlog from '../component/FullBlog'
 
 interface Blog{
   title: string;
@@ -15,14 +15,47 @@ interface Blog{
   id: string;
 }
 
-
 const Blog = () => {
-
   const {id} = useParams();
-  const {blog , loading} = useBlogs(id || "");
+  console.log(id)
+  const [loading , setLoading] = useState(true);
+  const [blog , setBlog] = useState<Blog>({
+    title: "",
+    content: "",
+    author: {
+      email: ""
+    },
+    id: ""
+  });
+
+  useEffect(() => {
+    async function hello(){
+      try{
+        const res = await axios.get(`http://127.0.0.1:8787/api/v1/blog/${id}` , {
+        headers: {
+          Authorization: localStorage.getItem("token")
+        }
+      })
+      setBlog(res.data.blog);
+      setLoading(false);
+      console.log(res.data.blog)
+      }catch(err){
+        console.log(err)
+      }
+    }
+    hello();
+  } , [id])
+
+  if(loading){
+    return <div className='flex justify-center items-center h-screen'>
+        <Loader />
+      </div>
+  }
   
   return (
-    <div>{blog}</div>
+    <div>
+      <FullBlog blog={blog}/>    
+    </div>
   )
 }
 
