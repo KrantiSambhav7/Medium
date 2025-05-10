5aa00d69c3cf66c193809c1b1237dce11627bf0e
KrantiSambhav7 <krantisambhav7@gmail.com>
2025-05-10 20:06:33 +0530

    Appbar updated

--- frontend/src/component/Appbar.tsx
+++ frontend/src/component/Appbar.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { Avatar } from './BlogCard'
-
+import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 const Appbar = ({name = "Harkirat"} : {name: string}) => {
+  const navigate = useNavigate();
   return (
     <div className='border-b border-slate-400 px-4 py-4 mb-8 flex justify-between items-center'>
-        <div className='font-bold text-xl'><h1>Medium</h1></div>
-        <Avatar name={"A"} size={8} />
+        <div onClick={() => {
+          navigate("/blogs");
+        }} className='font-bold text-xl cursor-pointer'><h1>Medium</h1></div>
+        <div className='flex items-center gap-10'>
+          <Link to={"/publish"}>
+            <button className='bg-green-400 px-6 rounded-full font-semibold cursor-pointer py-2 hover:bg-green-600'>Publish</button>
+          </Link>
+          <Avatar name={"A"} size={8} />
+        </div>
     </div>
   )
 }
