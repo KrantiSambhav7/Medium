ce0ac849389c8c48e7a025b9371b3c5d76bdb83e
KrantiSambhav7 <krantisambhav7@gmail.com>
2025-05-10 20:08:16 +0530

    index.ts updated

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -1,24 +1,2 @@
-import { useEffect, useState } from "react"
-import axios from "axios"
-import { BACKEND_URL } from "../config";
-
-export const useBlogs = (id : string ) => {
-    const [loading , setLoading] = useState(true);
-    const [blogs , setBlogs] = useState([]);
-    
-    useEffect(() => {
-        async () => {
-            const res = await axios.get(`${BACKEND_URL}/api/v1/blog/${id}` , {
-                headers: {
-                    Authorization: localStorage.getItem("token")
-                }
-            });
-            setLoading(false);
-            setBlogs(res.data.blog);
-        }
-    } , [])
-
-    return {
-        loading , blogs
-    }
+export const useBlogs = () => {
 }
