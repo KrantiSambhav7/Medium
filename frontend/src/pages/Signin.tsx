2f1052f52b25285bc2bd82e2da9a2d2779d1118f
KrantiSambhav7 <krantisambhav7@gmail.com>
2025-06-25 09:01:17 +0530

    Version of npm package updated

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -4,7 +4,7 @@ import Auth from '../component/Auth'
 const Signin = () => {
   return (
     <div>
-      <div className='grid grid-cols-1 lg:grid-cols-2'> 
+      <div className='grid grid-cols-1 lg:grid-cols-2 mt-30 md:mt-0'> 
         <Auth type={"signin"}/>
         <div className='hidden lg:block'>
         <Quote />
