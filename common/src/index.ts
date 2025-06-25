1db91642f902520a755f859d8f7e3fec52fb769f
KrantiSambhav7 <krantisambhav7@gmail.com>
2025-06-25 08:23:25 +0530

    npm publish

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,13 +1,13 @@
 import zod from "zod"
 
 export const signupInput = zod.object({
-    username: zod.string().email(),
+    email: zod.string().email(),
     password: zod.string().min(6),
     name: zod.string().optional()
  })
 
 export const signinInput = zod.object({
-   username: zod.string().email(),
+   email: zod.string().email(),
    password: zod.string().min(6),
 })
 
@@ -22,7 +22,7 @@ export const updateBlog = zod.object({
    id: zod.string()
 })
 
- //Frontend will need this to know what kind of the inouts he should send
+ //Frontend will need this to know what kind of the inputs should the developer use
  export type SignupInput = zod.infer<typeof signupInput>
  export type SigninInput = zod.infer<typeof signinInput>
  export type CreateBlog = zod.infer<typeof createBlog>
